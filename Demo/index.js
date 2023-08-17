a1ee0f5b619ca0b357ff1ae4659037301c930053
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-08-18 00:39:40 +0530

    go

---
+++ Demo/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World</h1>");
+
+})
+app.get("/contact", (req, res) => {
+    res.send("<h1>Contact Me</h1>9511891756");
+    res.send("");
+
+})
+
+app.get("/about", (req, res) => {
+    res.send("<h1>About Me</h1>");
+
+})
+
+//() callback just like lambda
+app.listen(port, () => {
+    console.log("Server RUnning on port " + port);
+})
